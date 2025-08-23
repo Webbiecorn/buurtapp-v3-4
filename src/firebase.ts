89070175f022b9f8715756e745dc7975ce51ce1b
Webbiecorn <webbiecorn@gmail.com>
2025-08-23 15:11:54 +0200

    Project opgelost en klaar voor backup

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDdNt987MgEFsi2AUsYTV-kyvXK_y9gmIs",
   authDomain: "buurtapp-v3-4.firebaseapp.com",
   projectId: "buurtapp-v3-4",
-  storageBucket: "buurtapp-v3-4.appspot.com",
+  storageBucket: "buurtapp-v3-4.firebasestorage.app",
   messagingSenderId: "740445217879",
   appId: "1:740445217879:web:afa1154ca0c5894a29af12"
 };
